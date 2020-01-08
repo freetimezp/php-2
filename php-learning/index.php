63d074f4f818facfd8bc1640a6c63deba4b0737c
freetimezp <kraftukrnet@ukr.net>
2020-01-08 21:22:55 +0200

    PhP.Learning,

--- php-learning/index.php
+++ php-learning/index.php
@@ -1,5 +1,22 @@
 <?php
 
+/* function */
+
+function printWords ($word,$a,$b) {
+	$word = math($a,$b);
+	echo "$word<hr />";
+}
+
+function math ($first, $second) {
+	$summa = $first + $second;
+	return $summa;
+}
+
+$x = 12;
+$y = 35;
+
+printWords($sum,$x,$y);
+
 
 
 
